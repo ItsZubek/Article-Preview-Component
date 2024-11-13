c285ca166ae018b5ef3d4a3f41e944c0792001e6
Zubek <mateuszsubiel@wp.pl>
2024-11-13 19:26:43 +0100

    finished the challenge

---
+++ script.js
@@ -0,0 +1,8 @@
+const sharebtn = document.querySelector('#sharebtn');
+const tooltip = document.querySelector('#tooltip');
+
+
+sharebtn.addEventListener('click', function() {
+tooltip.classList.toggle('invisible');
+
+});
